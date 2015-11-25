206404461397b1a04dda16493c2411134b950bc9
Mijkenator <mijkenator@gmail.com>
2015-11-25 15:06:28 -0500

    slight fixs

--- p9/p9_1.erl
+++ p9/p9_1.erl
@@ -4,17 +4,18 @@
 % vim:syn=erlang
 
 -mode(compile).
+-define(N, 1000).
 
 main(_) ->
     io:format("Answer: ~p ~n", [a(1)]).
 
-a(X) when X < 1000 ->
+a(X) when X < ?N ->
     case b(X,X) of
         false -> a(X+1);
         Ret   -> Ret
     end.
 
-b(X,Y) when Y < 1000, X*X+Y*Y =:= (1000-X-Y)*(1000-X-Y) -> X*Y*(1000-X-Y);
-b(X,Y) when Y < 1000 -> b(X,Y+1);
+b(X,Y) when Y < ?N, X*X+Y*Y =:= (?N-X-Y)*(?N-X-Y) -> X*Y*(?N-X-Y);
+b(X,Y) when Y < ?N -> b(X,Y+1);
 b(_,_) -> false.
 
